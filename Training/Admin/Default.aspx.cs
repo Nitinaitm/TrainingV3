57b40c810ea1f75ee6bb19c9ceac6f15f0b558f8
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-24 12:44:24 +0530

    Update Default.aspx.cs

--- Training/Admin/Default.aspx.cs
+++ Training/Admin/Default.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using OfficeOpenXml;
 
-namespace Training.Admin
+namespace Training.Admin 
 {
     public partial class Default : System.Web.UI.Page
     {
