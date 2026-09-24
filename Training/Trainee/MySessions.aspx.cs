65cbd1235a2abca51ff9ab3ec309c5b1b0bfb0ba
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-24 16:16:41 +0530

    Update MySessions.aspx.cs

--- Training/Trainee/MySessions.aspx.cs
+++ Training/Trainee/MySessions.aspx.cs
@@ -2,7 +2,7 @@ using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Web.UI.WebControls; 
 
 namespace Training.Trainee
 {
