01a2833e99f0f6978ff1e416c8764f0c587d7f00
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-25 15:18:15 +0530

    Add external trainee report designer

---
+++ Training/Admin/ExternalTraineeReport.aspx.designer.cs
@@ -0,0 +1,14 @@
+namespace Training.Admin
+{
+    public partial class ExternalTraineeReport
+    {
+        protected global::System.Web.UI.WebControls.TextBox txtEmpID;
+        protected global::System.Web.UI.WebControls.TextBox txtEmpName;
+        protected global::System.Web.UI.WebControls.TextBox txtCompany;
+        protected global::System.Web.UI.WebControls.TextBox txtDesignation;
+        protected global::System.Web.UI.WebControls.Button btnSearch;
+        protected global::System.Web.UI.WebControls.Button btnReset;
+        protected global::System.Web.UI.WebControls.Button btnExportExcel;
+        protected global::System.Web.UI.WebControls.GridView gvExternalTrainee;
+    }
+}
