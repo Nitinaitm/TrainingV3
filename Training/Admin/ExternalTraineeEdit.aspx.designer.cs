bbbf978b8a9876a6a7c646d36f83071adc1bf9b2
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-25 15:38:43 +0530

    Add external trainee edit designer

---
+++ Training/Admin/ExternalTraineeEdit.aspx.designer.cs
@@ -0,0 +1,15 @@
+namespace Training.Admin
+{
+    public partial class ExternalTraineeEdit
+    {
+        protected global::System.Web.UI.WebControls.TextBox txtEmpID;
+        protected global::System.Web.UI.WebControls.TextBox txtEmpName;
+        protected global::System.Web.UI.WebControls.TextBox txtMobileNo;
+        protected global::System.Web.UI.WebControls.TextBox txtEmailId;
+        protected global::System.Web.UI.WebControls.TextBox txtOrganization;
+        protected global::System.Web.UI.WebControls.TextBox txtDesignation;
+        protected global::System.Web.UI.WebControls.Button btnSave;
+        protected global::System.Web.UI.WebControls.Button btnCancel;
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+    }
+}
