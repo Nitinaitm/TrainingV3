2c2ab4925aefcc930282c9f50de30b34f68d009a
Nitinaitm <49182074+Nitinaitm@users.noreply.github.com>
2026-09-25 15:18:13 +0530

    Add external trainee report logic

---
+++ Training/Admin/ExternalTraineeReport.aspx.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using OfficeOpenXml;
+using System.Web.UI;
+
+namespace Training.Admin
+{
+    public partial class ExternalTraineeReport : System.Web.UI.Page
+    {
+        private clsDataAccess DB()
+        {
+            return new clsDataAccess();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            BindReport();
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtEmpID.Text = "";
+            txtEmpName.Text = "";
+            txtCompany.Text = "";
+            txtDesignation.Text = "";
+            gvExternalTrainee.DataSource = null;
+            gvExternalTrainee.DataBind();
+        }
+
+        private void BindReport()
+        {
+            string sql = "SELECT EmpID,EmpName,MobileNo,EmailId,EmpCompany,EmpDesignation,CreatedOn FROM EmpBasicMaster WHERE EmpType='External'";
+            sql += " AND (@EmpID='' OR EmpID LIKE @EmpID)";
+            sql += " AND (@EmpName='' OR EmpName LIKE @EmpName)";
+            sql += " AND (@Company='' OR EmpCompany LIKE @Company)";
+            sql += " AND (@Designation='' OR EmpDesignation LIKE @Designation)";
+            sql += " ORDER BY ID DESC";
+
+            DataTable dt = DB().GetDataTable(sql, new SqlParameter[] {
+                new SqlParameter("@EmpID", "%" + txtEmpID.Text.Trim() + "%"),
+                new SqlParameter("@EmpName", "%" + txtEmpName.Text.Trim() + "%"),
+                new SqlParameter("@Company", "%" + txtCompany.Text.Trim() + "%"),
+                new SqlParameter("@Designation", "%" + txtDesignation.Text.Trim() + "%")
+            });
+
+            gvExternalTrainee.DataSource = dt;
+            gvExternalTrainee.DataBind();
+        }
+
+        protected void btnExportExcel_Click(object sender, EventArgs e)
+        {
+            BindReport();
+
+            if (gvExternalTrainee.Rows.Count == 0)
+            {
+                return;
+            }
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("External Trainee");
+                for (int i = 0; i < gvExternalTrainee.HeaderRow.Cells.Count; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = gvExternalTrainee.HeaderRow.Cells[i].Text;
+                }
+
+                for (int rowIndex = 0; rowIndex < gvExternalTrainee.Rows.Count; rowIndex++)
+                {
+                    for (int cellIndex = 0; cellIndex < gvExternalTrainee.Rows[rowIndex].Cells.Count; cellIndex++)
+                    {
+                        worksheet.Cells[rowIndex + 2, cellIndex + 1].Value = Server.HtmlDecode(gvExternalTrainee.Rows[rowIndex].Cells[cellIndex].Text);
+                    }
+                }
+
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+                Response.Clear();
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.AddHeader("content-disposition", "attachment; filename=ExternalTraineeReport.xlsx");
+                Response.BinaryWrite(package.GetAsByteArray());
+                Response.End();
+            }
+        }
+    }
+}
